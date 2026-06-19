28fb35b6deedd6649b0beb7630ff221c2cb9f7c6
Dioumadev221 <dioumadione004@gmail.com>
2026-06-19 13:24:36 +0000

    commit apres arret temporere

--- lib/shared/layout/main_scaffold.dart
+++ lib/shared/layout/main_scaffold.dart
@@ -129,7 +129,7 @@ class _NavItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final color =
-        isActive ? AppColors.primary : const Color(0xFF94A3B8);
+        isActive ? AppColors.primary : const Color(0xFF475569);
     return GestureDetector(
       onTap: onTap,
       behavior: HitTestBehavior.opaque,
